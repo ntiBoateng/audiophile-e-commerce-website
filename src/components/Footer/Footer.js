966b6a049b8fa5beb74932237ba06ca9f37fdd00
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-04 17:46:23 +0000

    "Done with footer.js"

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,40 +1,40 @@
 import React from "react";
 import logo from "../../assets/shared/desktop/logo.svg";
 import {Link} from "react-router-dom";
-// import SocialMedia from "../SocialMedia";
+import SocialMedia from "../SocialMedia";
 
 function Footer(){
     return(
-        <footer>
-            <section>
-                <section>
-                    <article>
+        <footer className="footer">
+            <section className="footer-container">
+                <section className="footer-nav">
+                    <article className="footer-logo">
                         <Link to="/">
                             <img src={logo} alt="AudioPhile E-Commerce Website"/>
                         </Link>
                     </article>
-                    <article>
-                        <section>
-                            <Link>Home</Link>
-                            <Link>Headphones</Link>
-                            <Link>Speakers</Link>
-                            <Link>Earphones</Link>
+                    <article className="footer-links">
+                        <section className="navLinksContainerFooter">
+                            <Link to="/">Home</Link>
+                            <Link to="/speakers">Headphones</Link>
+                            <Link to="/headphones">Speakers</Link>
+                            <Link to="/earphones">Earphones</Link>
                         </section>
                     </article>
                 </section>
-                <article>
+                <article className="footer-info">
                     <p>
                         Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers
                         and sound specialists who are devoted to helping you get the most out of personal audio. Come and
                         visit our demo facility - we’re open 7 days a week.
                     </p>
-                    {/* <SocialMedia/> */}
+                    <SocialMedia classed={"footer-medias tablet"} />
                 </article>
-                <article>
-                    <aside>
+                <article className="footer-copyright">
+                    <aside className="copyright">
                         <p>Copyright 2021. All Rights Reserved</p>
                     </aside>
-                    {/* <SocialMedia/> */}
+                    <SocialMedia classed={"footer-medias desktop"} />
                 </article>
             </section>
         </footer>
