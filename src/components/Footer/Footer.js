934ac6d96c02e8e09fdf3dce683f9f7ec978e988
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-04 14:25:59 +0000

    adding tags in Footer.js file

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/shared/desktop/logo.svg";
+import {link} from "react-router-dom";
+import SocialMedia from "../SocialMedia";
+
+function Footer(){
+    return(
+        <footer>
+            <section>
+                <section>
+                    <article></article>
+                    <article></article>
+                </section>
+                <article></article>
+                <article></article>
+            </section>
+        </footer>
+    );    
+}
+
+export default Footer;
+// Home
+// Headphones
+// Speakers
+// Earphones
+
+// Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers 
+// and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
+// visit our demo facility - we’re open 7 days a week.
+
+// Copyright 2021. All Rights Reserved
