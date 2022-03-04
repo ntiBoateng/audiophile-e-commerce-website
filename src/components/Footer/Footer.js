280dc20e1c229e0104733de726bdc724cc0a52e4
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-04 15:58:20 +0000

    "added some text in the footer.js file"

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/shared/desktop/logo.svg";
-import {link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import SocialMedia from "../SocialMedia";
 
 function Footer(){
@@ -8,24 +8,37 @@ function Footer(){
         <footer>
             <section>
                 <section>
-                    <article></article>
-                    <article></article>
+                    <article>
+                        <Link to="/">
+                            <img src={logo} alt="AudioPhile E-Commerce Website"/>
+                        </Link>
+                    </article>
+                    <article>
+                        <section>
+                            <Link>Home</Link>
+                            <Link>Headphones</Link>
+                            <Link>Speakers</Link>
+                            <Link>Earphones</Link>
+                        </section>
+                    </article>
                 </section>
-                <article></article>
-                <article></article>
+                <article>
+                    <p>
+                        Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers
+                        and sound specialists who are devoted to helping you get the most out of personal audio. Come and
+                        visit our demo facility - we’re open 7 days a week.
+                    </p>
+                    <SocialMedia/>
+                </article>
+                <article>
+                    <aside>
+                        <p>Copyright 2021. All Rights Reserved</p>
+                    </aside>
+                    <SocialMedia/>
+                </article>
             </section>
         </footer>
     );    
 }
 
 export default Footer;
-// Home
-// Headphones
-// Speakers
-// Earphones
-
-// Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers 
-// and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
-// visit our demo facility - we’re open 7 days a week.
-
-// Copyright 2021. All Rights Reserved
