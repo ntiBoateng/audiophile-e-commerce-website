ca73574c8654ce2a24d7f9171f5d04c550f84921
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-04 18:54:28 -0800

    page structuring

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,68 @@
-
-
-
-export function Navbar(){
-
+function Navbar() {
     const {
-        isNavOpen,
-        setIsNavOpen,
-        setIsCartOpen,
-        isView,
-        isCartOpen,
-        CartInfo,
-      } = useGlobalContext();
-
-
+      isNavOpen,
+      setIsNavOpen,
+      setIsCartOpen,
+      isView,
+      isCartOpen,
+      CartInfo,
+    } = useGlobalContext();
+  
     return (
-<nav className="nav">
-
-
-
-
-</nav>
-    )
-}
-
+      <nav className="nav">
+        <section
+          className={`${
+            isNavOpen || isCartOpen || isView
+              ? "nav-center"
+              : "nav-center nav-active"
+          }`}
+        >
+          <section className="nav-logo">
+            <div
+              onClick={() => setIsNavOpen(!isNavOpen)}
+              className={`${isNavOpen ? "bars is-active" : "bars"}`}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="logo">
+              <Link onClick={() => setIsNavOpen(false)} to="/">
+                <img src={logo} alt="AudioPhile-Logo" />
+              </Link>
+            </div>
+          </section>
+  
+          <section className="navLinksContainer">
+            <Link to="/">Home</Link>
+            <Link to="/speakers">Speaker</Link>
+            <Link to="/headphones">HeadPhone</Link>
+            <Link to="/earphones">EarPhone</Link>
+          </section>
+  
+          <section
+            className={`${
+              isNavOpen ? "mobileNavLinks shownavs" : "mobileNavLinks"
+            }`}
+          >
+            <Navs />
+          </section>
+  
+          <section onClick={() => setIsCartOpen(!isCartOpen)} className="cart">
+            <img src={cartIcon} alt="Cart-Image" />
+            <p onClick={() => setIsCartOpen(!isCartOpen)} className="amount">
+              {CartInfo.length}
+            </p>
+          </section>
+  
+          <section
+            className={`${
+              isCartOpen ? "cart-container show-cart-container" : "cart-container"
+            }`}
+          >
+            <Cart />
+          </section>
+        </section>
+      </nav>
+    );
+  }
