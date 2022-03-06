a47618fe0b2881e7464a33d731b744b4c96e837d
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-06 20:01:52 +0000

    "Added some tags"

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -5,4 +5,16 @@ import {Link} from "react-router-dom";
 function Like({Products}){
     const {others, category} = Products;
     const {getWidth} = useGlobalContext();
+    return(
+        <section>
+            <div>
+                <h2></h2>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    );
 }
+
+export default Like;
