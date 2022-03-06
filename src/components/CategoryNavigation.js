d99e4555e3445483b96c18f39a8ed4f7a186a269
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-06 23:01:09 +0000

    "Tags added"

---
+++ src/components/CategoryNavigation.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function CategoryNavigation(){
+    return(
+        <sectio>
+            <section>
+                <Link>
+                    <div>
+                        <div>
+                            <img/>
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div>
+                            <p>
+                                <span>
+                                    <img/>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+                <Link>
+                    <div>
+                        <div>
+                            <img/>
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div>
+                            <p>
+                                <span>
+                                    <img/>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+                <Link>
+                    <div>
+                        <div>
+                            <img/>
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div>
+                            <p>
+                                <span>
+                                    <img/>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+            </section>
+        </sectio>
+    );
+}
+
+export default CategoryNavigation;
