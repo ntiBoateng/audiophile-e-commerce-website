11840fa54be9c649522c9fca63e210072e8c4bee
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-06 19:48:28 +0000

    "Started working on Like.js file"

---
+++ src/components/Like/Like.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {useGlobalContext} from "../../context";
+import {Link} from "react-router-dom";
+
+function Like({Products}){
+    const {others, category} = Products;
+    const {getWidth} = useGlobalContext();
+}
