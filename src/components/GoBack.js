fe3dedb18e0543c16c5d715facfabfe379e0de56
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-06 22:46:29 +0000

    "Added code for Goback.js file"

---
+++ src/components/GoBack.js
@@ -0,0 +1,12 @@
+import React from "react";
+function GoBack({history}){
+    return(
+        <section data-aos="fade-left" className="go-back">
+            <div className="contain">
+                <button onClick={history.goBack}>Go Back</button>
+            </div>
+        </section>
+    );
+}
+
+export default GoBack;
