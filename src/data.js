468d5ead198d90efb7963a649163b96b518d495b
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 18:03:46 +0000

    "Creating lists of items in data.js"

--- src/data.js
+++ src/data.js
@@ -141,3 +141,93 @@ import zx9_speaker_galleries_second_desktop from "./assets/product-zx9-speaker/d
 import zx9_speaker_galleries_three_mobile from "./assets/product-zx9-speaker/mobile/image-gallery-3.jpg";
 import zx9_speaker_galleries_three_tablet from "./assets/product-zx9-speaker/tablet/image-gallery-3.jpg";
 import zx9_speaker_galleries_three_desktop from "./assets/product-zx9-speaker/desktop/image-gallery-3.jpg";
+
+const data = [
+    {
+        id: 1,
+        slug: "yx1-earphones",
+        name: "YX1 Wireless Earphones",
+        image: {
+          mobile: yx1_earphones_mobile,
+          tablet: yx1_earphones_tablet,
+          desktop: yx1_earphones_desktop,
+        },
+        category: "earphones",
+        new: true,
+        price: 599,
+        description:
+          "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
+        features:
+          "Experience unrivalled stereo sound thanks to innovative acoustic technology. With improved ergonomics designed for full day wearing, these revolutionary earphones have been finely crafted to provide you with the perfect fit, delivering complete comfort all day long while enjoying exceptional noise isolation and truly immersive sound.\n\nThe YX1 Wireless Earphones features customizable controls for volume, music, calls, and voice assistants built into both earbuds. The new 7-hour battery life can be extended up to 28 hours with the charging case, giving you uninterrupted play time. Exquisite craftsmanship with a splash resistant design now available in an all new white and grey color scheme as well as the popular classic black.",
+        includes: [
+          {
+            quantity: 2,
+            item: "Earphone unit",
+          },
+          {
+            quantity: 6,
+            item: "Multi-size earplugs",
+          },
+          {
+            quantity: 1,
+            item: "User manual",
+          },
+          {
+            quantity: 1,
+            item: "USB-C charging cable",
+          },
+          {
+            quantity: 1,
+            item: "Travel pouch",
+          },
+        ],
+        gallery: {
+          first: {
+            mobile: yx1_earphones_mobile_galleries_first,
+            tablet: yx1_earphones_tablet_galleries_first,
+            desktop: yx1_earphones_desktop_galleries_first,
+          },
+          second: {
+            mobile: yx1_earphones_mobile_galleries_second,
+            tablet: yx1_earphones_tablet_galleries_second,
+            desktop: yx1_earphones_desktop_galleries_second,
+          },
+          third: {
+            mobile: yx1_earphones_mobile_galleries_third,
+            tablet: yx1_earphones_tablet_galleries_third,
+            desktop: yx1_earphones_desktop_galleries_third,
+          },
+        },
+        others: [
+          {
+            slug: "xx99-mark-one-headphones",
+            name: "XX99 Mark I",
+            image: {
+              mobile: xx99_mark_one_headphones_mobile,
+              tablet: xx99_mark_one_headphones_tablet,
+              desktop: xx99_mark_one_headphones_desktop,
+            },
+          },
+          {
+            slug: "xx59-headphones",
+            name: "XX59",
+            image: {
+              mobile: xx59_headphones_mobile,
+              tablet: xx59_headphones_tablet,
+              desktop: xx59_headphones_desktop,
+            },
+          },
+          {
+            slug: "zx9-speaker",
+            name: "ZX9 Speaker",
+            image: {
+              mobile: zx9_speaker_mobile,
+              tablet: zx9_speaker_mobile,
+              desktop: zx9_speaker_mobile,
+            },
+          },
+        ],
+      },
+];
+
+export default data;
