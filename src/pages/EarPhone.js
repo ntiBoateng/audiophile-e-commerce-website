7c0fc9960be7b0d3612c080928a50df46952588a
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 09:36:54 +0000

    "Added coding logic to EarPhone.js"

--- src/pages/EarPhone.js
+++ src/pages/EarPhone.js
@@ -1,6 +1,49 @@
 import React from "react";
+import Hero from "../components/ProductHero/Hero";
+import DetailsCategory from "../components/ProductHero/DetailsCategory";
+import { Link } from "react-router-dom";
+import { category } from "../dataCategory";
+import CategoryNavigation from "../components/CategoryNavigation";
+import BestGear from "../components/BestGear";
+import { useGlobalContext } from "../context";
 
 function EarPhone(){
     const {earphones} = category;
     const {getWidth} = useGlobalContext();
+
+    return(
+        <>
+      <Hero hero="black-background" title="Earphones" />
+      {earphones.map((earphone, index) => {
+        const { newProduct, name, slug, info, url, image } = earphone;
+        return (
+          <DetailsCategory
+            classInfo={`${
+              index % 2 === 1
+                ? "category-container category-container-inverse"
+                : "category-container"
+            }`}
+            newProduct={newProduct}
+            name={name}
+            info={info}
+            url={url}
+            image={
+              getWidth <= 600
+                ? image.mobile
+                : getWidth > 600 || getWidth <= 768
+                ? image.tablet
+                : image.desktop
+            }
+            key={index}
+          >
+            <Link to={`/earphones/${slug}`} className="btns orange-background">
+              SEE PRODUCT
+            </Link>
+          </DetailsCategory>
+        );
+      })}
+      <CategoryNavigation />
+      <BestGear />
+    </>
+    );
 }
