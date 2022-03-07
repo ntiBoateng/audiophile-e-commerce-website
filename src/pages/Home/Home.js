6fbb9d5aec728193fb9a912b7aa8beb748306e96
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 07:29:22 +0000

    "Tags added to Home.js"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BestGear from "../../components/BestGear";
 import CategoryNavigation from "../../components/CategoryNavigation";
 
 function Home(){
@@ -30,6 +31,29 @@ function Home(){
                     </div>
                 </section>
             </section>
+            <section>
+                <section>
+                    <article>
+                        <h2></h2>
+                        <Link>
+                            <button></button>
+                        </Link>
+                    </article>
+                </section>
+            </section>
+            <section>
+                <section>
+                    <article></article>
+                    <article>
+                        <h2></h2>
+                        <Link>
+                            <button></button>
+                        </Link>
+                    </article>
+                </section>
+            </section>
+
+            <BestGear />
         </>
     );
 }
