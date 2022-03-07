061b5075483b25331e93f8ed4398fad473d099b9
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 07:19:40 +0000

    "Adding tags in EarPhone.js"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import CategoryNavigation from "../../components/CategoryNavigation";
 
 function Home(){
-
+    return(
+        <>
+            <section>
+                <section>
+                    <section>
+                        <h4></h4>
+                        <h1></h1>
+                        <p></p>
+                        <Link></Link>
+                    </section>
+                </section>
+            </section>
+            <CategoryNavigation />
+            <section>
+                <section>
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1></h1>
+                            <p></p>
+                            <Link></Link>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </>
+    );
 }
 
 export default Home;
