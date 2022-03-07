b2e24a80b009c4e261f9db7fed3e19ba5feb5f63
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 17:58:19 +0000

    "Importing pictures in data.js"

---
+++ src/data.js
@@ -0,0 +1,8 @@
+import yx1_earphones_mobile from "./assets/product-yx1-earphones/mobile/image-product.jpg";
+import yx1_earphones_tablet from "./assets/product-yx1-earphones/tablet/image-product.jpg";
+import yx1_earphones_desktop from "./assets/product-yx1-earphones/desktop/image-product.jpg";
+
+///yx1 first galleries
+import yx1_earphones_mobile_galleries_first from "./assets/product-yx1-earphones/mobile/image-gallery-1.jpg";
+import yx1_earphones_tablet_galleries_first from "./assets/product-yx1-earphones/tablet/image-gallery-1.jpg";
+import yx1_earphones_desktop_galleries_first from "./assets/product-yx1-earphones/desktop/image-gallery-1.jpg";
