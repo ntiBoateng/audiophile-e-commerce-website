c3db1fda92ff568b160f5cee21aa9c34ab68d04c
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 08:28:25 +0000

    "Attributes added in Home.js"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,29 +10,34 @@ function Home(){
                 <section className="home-container">
                     <section className="home-container-info">
                         <h4 data-aos="fade-left" data-aos-delay="200">NEW PRODUCT</h4>
-                        <h1>
+                        <h1 data-aos="fade-left" data-aos-delay="400">
                             XX99 MARK II
                             <br />
                             HEADPHONES
                         </h1>
-                        <p>
+                        <p data-aos="fade-left" data-aos-delay="600">
                         Experience natural, lifelike audio and exceptional build quality
                         made for the passionate music enthusiast.
                         </p>
-                        <Link>
+                        <Link
+                            data-aos="fade-left"
+                            data-aos-delay="800"
+                            className="home-btn btns"
+                            to="/headphones/xx99-mark-two-headphones"
+                            >
                             SEE PRODUCT
                         </Link>
                     </section>
                 </section>
             </section>
             <CategoryNavigation />
-            <section>
-                <section>
-                    <div>
-                        <div></div>
+            <section className="speaker-Z">
+                <section className="speaker-Z-container">
+                    <div data-aos="fade-down" className="speaker-img">
+                        <div className="img-speaker"></div>
                     </div>
-                    <div>
-                        <div>
+                    <div  data-aos="fade-up" className="spkInfo">
+                        <div className="speaker-info">
                             <h1>
                                 ZX9
                                 <br />
@@ -42,30 +47,35 @@ function Home(){
                                 Upgrade to premium speakers that are phenomenally built to
                                 deliver truly remarkable sound.
                             </p>
-                            <Link>SEE PRODUCT</Link>
+                            <Link to="/speakers/zx9-speaker" className="product-btn btns">SEE PRODUCT</Link>
                         </div>
                     </div>
                 </section>
             </section>
-            <section>
-                <section>
-                    <article>
-                        <h2>ZX7 SPEAKER</h2>
-                        <Link>
-                            <button>SEE PRODUCT</button>
+            <section className="section-Z7">
+                <section className="Z7-container">
+                    <article data-aos="fade-left" className="Z7-info">
+                        <h2 data-aos="fade-right" data-aos-delay="200">ZX7 SPEAKER</h2>
+                        <Link to="/speakers/zx7-speaker">
+                            <button data-aos="fade-right" 
+                            data-aos-delay="400" 
+                            className="btns Z7-btn">SEE PRODUCT</button>
                         </Link>
                     </article>
                 </section>
             </section>
-            <section>
-                <section>
-                    <article></article>
-                    <article>
-                        <h2>
+            <section className="earphone-Z">
+                <section className="earphone-Z-container">
+                    <article data-aos="fade-down" className="Z-earphone"></article>
+                    <article data-aos="fade-up" className="earphone-container-info">
+                        <h2 data-aos="fade-left" data-aos-delay="200">
                             YX1 EARPHONES
                         </h2>
-                        <Link>
-                            <button>SEE PRODUCT</button>
+                        <Link 
+                            to="/earphones/yx1-earphones"
+                            data-aos="fade-left"
+                            data-aos-delay="400">
+                            <button className="btns earphone-btn">SEE PRODUCT</button>
                         </Link>
                     </article>
                 </section>
