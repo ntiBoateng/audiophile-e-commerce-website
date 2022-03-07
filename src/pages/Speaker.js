6311ec51cb5ac8bba03a39c69cc94734d8a18451
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-06 16:38:21 -0800

    speakers page

---
+++ src/pages/Speaker.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hero from "../components/ProductHero/Hero";
+import DetailsCategory from "../components/ProductHero/DetailsCategory";
+import { Link } from "react-router-dom";
+import { category } from "../dataCategory";
+import CategoryNavigation from "../components/CategoryNavigation";
+import BestGear from "../components/BestGear";
+import { useGlobalContext } from "../context";
