37f54c5dd481792f25f344dd0f9482b1adeb616b
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-07 18:15:12 +0000

    "Importing pictures to datacategory.js"

---
+++ src/datacategory.js
@@ -0,0 +1,30 @@
+//mark 11 headpphones
+
+import headPhoneMobile0 from "./assets/category-headphones/mobile/image-xx99-mark-two.jpg";
+import headPhoneTablet0 from "./assets/category-headphones/tablet/image-xx99-mark-two.jpg";
+import headPhoneDesktop0 from "./assets/category-headphones/desktop/image-xx99-mark-two.jpg";
+
+//mark 1 headphone
+import headPhoneMobile1 from "./assets/category-headphones/mobile/image-xx99-mark-one.jpg";
+import headPhoneTablet1 from "./assets/category-headphones/tablet/image-xx99-mark-one.jpg";
+import headPhoneDesktop1 from "./assets/category-headphones/desktop/image-xx99-mark-one.jpg";
+
+//xx59 headphones
+import headPhoneMobile2 from "./assets/category-headphones/mobile/image-xx59.jpg";
+import headPhoneTablet2 from "./assets/category-headphones/tablet/image-xx59.jpg";
+import headPhoneDesktop2 from "./assets/category-headphones/desktop/image-xx59.jpg";
+
+//speakers ZX9
+import speakerMobile0 from "./assets/category-speakers/mobile/image-zx9.jpg";
+import speakerTablet0 from "./assets/category-speakers/tablet/image-zx9.jpg";
+import speakerDesktop0 from "./assets/category-speakers/desktop/image-zx9.jpg";
+
+//speakers ZX7
+import speakerMobile1 from "./assets/category-speakers/mobile/image-zx7.jpg";
+import speakerTablet1 from "./assets/category-speakers/tablet/image-zx7.jpg";
+import speakerDesktop1 from "./assets/category-speakers/desktop/image-zx7.jpg";
+
+//earphopnes
+import earphoneMobile from "./assets/category-earphones/mobile/image-yx1-earphones.jpg";
+import earphoneTablet from "./assets/category-earphones/tablet/image-yx1-earphones.jpg";
+import earphoneDesktop from "./assets/category-earphones/desktop/image-yx1-earphones.jpg";
